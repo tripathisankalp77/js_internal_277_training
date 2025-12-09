cdddf69ce4470effad85f51515d62e54222016e5
tripathisankalp77 <tripathisankalp27@gmail.com>
2025-12-09 13:32:51 +0000

    yes

---
+++ day02/day02.js
@@ -0,0 +1,22 @@
+let a="abc"
+let b=""
+let c=null
+let d;
+let e=true
+console.log("value of a "+ a)
+console.log("value of b "+b)
+console.log("value of c "+c)
+console.log("value of d "+d)
+console.log("value of e "+e)
+console.log(typeof(a))
+console.log(typeof(undefined))
+console.log(typeof(null))
+
+
+let score="33abc"
+console.log(typeof(score))
+console.log(typeof score)
+//typecasting 
+let vmane=Number(score)
+console.log(typeof(vmane))
+console.log((vmane))
