1e7a61e215829398f6fe057a56b915473fe70b6a
tripathisankalp77 <tripathisankalp27@gmail.com>
2025-12-11 05:32:33 +0000

    day01

--- day01/first.js
+++ day01/first.js
@@ -1,2 +1,9 @@
 console.log("Sankalptripathi")
 console.log("first program in js")
+let mydate=new Date
+console.log(mydate.toString())
+console.log(mydate.toLocaleDateString())
+console.log(mydate.toLocaleTimeString())
+console.log(mydate.toISOString())
+console.log(mydate.getHours())
+console.log(mydate.getFullYear())
