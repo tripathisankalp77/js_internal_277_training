09190cb2ffa58b6ba6a481492f8f0b563f42f563
tripathisankalp77 <tripathisankalp27@gmail.com>
2025-12-21 09:29:39 +0000

    yes

--- Evaluationfile/obj/Debug/net8.0/csp1.AssemblyInfo.cs
+++ Evaluationfile/obj/Debug/net8.0/csp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8451ca064d915811d6e7db22904183271e089e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ae1d5bfba4b7bef2da47967c483e5af35fa2dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("csp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
